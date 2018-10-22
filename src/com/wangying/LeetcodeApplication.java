e30980158f6a13bb9494f3d60472698081080f08
wanyging.dz3 <wangying.dz3@founder.com.cn>
2018-10-22 15:02:13 +0800

    first commit

---
+++ src/com/wangying/LeetcodeApplication.java
@@ -0,0 +1,18 @@
+package com.wangying;
+
+import com.wangying.leetcode.leetCodeSolution;
+
+public class LeetcodeApplication {
+
+	
+	public static void main(String[] args) {
+		
+		
+		String []  source = new String [] {"a"};
+		
+		System.out.println(leetCodeSolution.longestCommonPrefix(source));
+		
+		
+	}
+	
+}
